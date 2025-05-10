447d0bd6de6e5aa0c1842427cf44bd5e5ac4c4cc
zhuchangbin <zhuchangbin@tuyoogame.com>
2025-05-10 10:53:57 +0800

    redis

---
+++ ape-common/ape-common-redis/src/main/java/com/ckzcb/ape/common/redis/utils/RedisSet.java
@@ -0,0 +1,15 @@
+package com.ckzcb.ape.common.redis.utils;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @ClassName RedisSet
+ * @Description
+ * @Author 蛋白质先生
+ * @Date 2025/4/21 15:30
+ * @Version 1.0
+ */
+@Component
+public class RedisSet {
+    
+}
