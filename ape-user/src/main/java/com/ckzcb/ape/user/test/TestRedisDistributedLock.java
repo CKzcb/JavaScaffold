8d2083283181ad13a9915d3d65cb2d4530caa783
zhuchangbin <zhuchangbin@tuyoogame.com>
2025-05-10 17:49:15 +0800

    redis分布式锁测试

---
+++ ape-user/src/main/java/com/ckzcb/ape/user/test/TestRedisDistributedLock.java
@@ -0,0 +1,28 @@
+package com.ckzcb.ape.user.test;
+
+import com.ckzcb.ape.common.redis.utils.RedisDistributedLock;
+import jakarta.annotation.Resource;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @ClassName TestRedisDistributedLock
+ * @Description
+ * @Author 蛋白质先生
+ * @Date 2025/5/10 17:28
+ * @Version 1.0
+ */
+@Component
+public class TestRedisDistributedLock implements CommandLineRunner {
+    @Resource
+    private RedisDistributedLock redisDistributedLock;
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println(redisDistributedLock.lock("zzzz", "1", 10, TimeUnit.SECONDS));
+        System.out.println(redisDistributedLock.lock("zzzz", "1", 10, TimeUnit.SECONDS));
+        System.out.println(redisDistributedLock.spinLock("zzzz", "1", 10, TimeUnit.SECONDS));
+    }
+}
