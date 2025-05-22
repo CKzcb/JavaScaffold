f543dd0c2d10e1b4bd9f46d1e5ce1414855b04cb
zhuchangbin <zhuchangbin@tuyoogame.com>
2025-05-22 09:55:53 +0800

    日志 aop

---
+++ ape-common/ape-common-log/src/main/java/com/ckzcb/ape/common/log/LogAspect.java
@@ -0,0 +1,45 @@
+package com.ckzcb.ape.common.log;
+
+import jakarta.annotation.PostConstruct;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.stereotype.Component;
+
+/**
+ * @ClassName LogAspect
+ * @Description
+ * @Author 蛋白质先生
+ * @Date 2025/5/21 22:25
+ * @Version 1.0
+ */
+@Aspect
+@Component
+@ConditionalOnProperty(prefix = "log", name = "enable", havingValue = "true", matchIfMissing = true)
+public class LogAspect {
+    private static final Logger logger = LoggerFactory.getLogger(LogAspect.class);
+
+    // 修正包路径：匹配任意层级的 controller 包
+    @Pointcut("execution(* com.ckzcb.ape..controller.*Controller.*(..))")
+    private void pointCut() {
+    }
+
+    @PostConstruct
+    public void init() {
+        logger.info(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>---");
+    }
+
+    // 修正环绕通知
+    @Around("pointCut()")
+    public Object around(ProceedingJoinPoint joinPoint) throws Throwable {
+        logger.info(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>---");
+        System.out.println("+++++++++++++++");
+
+        // 调用目标方法并返回结果
+        return joinPoint.proceed();
+    }
+}
